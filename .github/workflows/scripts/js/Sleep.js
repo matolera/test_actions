a564d9f6c2e7b5433690c1c3832e32272e9b0ed5
matolera <matiaslera@gmail.com>
2022-04-28 22:47:33 -0300

    Update Sleep.js

--- .github/workflows/scripts/js/Sleep.js
+++ .github/workflows/scripts/js/Sleep.js
@@ -1,11 +1,22 @@
-module.exports = (milliseconds) => {
-    sleep(milliseconds);
+const method = () => {
+  // your method logic
+  console.log('method')
+}
 
-    function sleep(milliseconds) {
-      const date = Date.now();
-      let currentDate = null;
-      do {
-        currentDate = Date.now();
-      } while (currentDate - date < milliseconds);
-    }
+const otherMethod = () => {
+  // your method logic 
+  console.log('other method')
+}
+
+function sleep(seconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < (seconds * 1000));
+}
+
+module.exports = (milliseconds) => {
+  method, 
+  otherMethod
 }
