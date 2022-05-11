1e5f3550961e406161d41991426ef170f4fea4ce
matolera <matiaslera@gmail.com>
2022-05-11 10:56:56 -0300

    Update Sleep.js

--- .github/workflows/scripts/js/Sleep.js
+++ .github/workflows/scripts/js/Sleep.js
@@ -11,7 +11,7 @@ const createWorkflowDispatch = async (github, ref, context, params) => {
 const checkStatus = async (github, context, core) => {
   let currentStatus = null;
   let conclusion = null;
-  sleep(2000)
+//  sleep(2000)
 
   do {
     let workflowLog = await github.rest.actions.listWorkflowRuns({
@@ -30,7 +30,7 @@ const checkStatus = async (github, context, core) => {
     }
 
     console.log('export-unpack-commit-solution status: ' + currentStatus)
-    sleep(20000)
+    await waitUntil(() => currentStatus != 'completed')
   } while (currentStatus != 'completed');
 
   if (conclusion != 'success') {
@@ -51,6 +51,50 @@ const checkEnvironment = async (github, context, environment) => {
       core.setFailed('The Environment does not exists!')
     }
   }
+}
+
+function test() {
+  let workflowLog = await github.rest.actions.listWorkflowRuns({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    workflow_id: 'export-unpack-commit-solution.yml',
+    per_page: 1
+  })
+
+  if (workflowLog.data.total_count > 0) {
+    currentStatus = workflowLog.data.workflow_runs[0].status
+    conclusion = workflowLog.data.workflow_runs[0].conclusion
+  }
+}
+
+const waitUntil = (condition) => {
+  console.log('waitUntil')
+  return new Promise((resolve) => {
+      let interval = setInterval(() => {
+          if (!condition()) {
+              console.log('!condition')
+              return
+          }
+
+          clearInterval(interval)
+          resolve()
+          console.log('resolve')
+      }, 100)
+  })
+}
+
+function until(conditionFunction) {
+
+  const poll = resolve => {
+    if(conditionFunction()) resolve();
+    else setTimeout(_ => poll(resolve), 10000);
+  }
+
+  return new Promise(poll);
+}
+
+const sleep2 = (milliseconds) => {
+
 }
 
 const sleep = (milliseconds) => {
